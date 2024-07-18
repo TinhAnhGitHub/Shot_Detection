4a041875dfcb1ceda97593a8cb36d4a7a0f1515a
Tinh Anh <anh.nguyennhu2306@hcmut.edu.vn>
2024-07-18 16:07:28 +0700

    changing requirements.txt, changing drive uploader for server)

--- drive_loader.py
+++ drive_loader.py
@@ -1,19 +1,35 @@
 import os
 import pickle
-from google_auth_oauthlib.flow import Flow, InstalledAppFlow # for AUTH2 flow
-from googleapiclient.discovery import build # for building API services
-from googleapiclient.http import MediaFileUpload # for uploading media files
-from google.auth.transport.requests import Request # for refreshing credentials
+from google.oauth2.credentials import Credentials
+from google.oauth2 import service_account
+from google_auth_oauthlib.flow import Flow, InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+from google.auth.transport.requests import Request
 
 class DriveUploader:
-    def __init__(self, credentials_path: str, token_path: str):
+    def __init__(self, credentials_path: str, token_path: str = None, use_service_account: bool = False):
         self.credentials_path = credentials_path
         self.token_path = token_path
+        self.use_service_account = use_service_account
         self.service = self._authenticate()
 
     def _authenticate(self):
+        if self.use_service_account:
+            return self._authenticate_service_account()
+        else:
+            return self._authenticate_oauth()
+
+    def _authenticate_service_account(self):
+        credentials = service_account.Credentials.from_service_account_file(
+            self.credentials_path,
+            scopes=['https://www.googleapis.com/auth/drive.file']
+        )
+        return build('drive', 'v3', credentials=credentials)
+
+    def _authenticate_oauth(self):
         creds = None
-        if os.path.exists(self.token_path):
+        if self.token_path and os.path.exists(self.token_path):
             with open(self.token_path, 'rb') as token:
                 creds = pickle.load(token)
         
@@ -25,21 +41,17 @@ class DriveUploader:
                     self.credentials_path, ['https://www.googleapis.com/auth/drive.file'])
                 creds = flow.run_local_server(port=0)
             
-            with open(self.token_path, 'wb') as token:
-                pickle.dump(creds, token)
+            if self.token_path:
+                with open(self.token_path, 'wb') as token:
+                    pickle.dump(creds, token)
 
         return build('drive', 'v3', credentials=creds)
 
     def create_folder_tree(self, base_folder_id: str, path: str) -> str:
-        """
-        Create a folder tree in Google Drive and return the ID of the deepest folder.
-        It's like planting a goddamn family tree, but for your files.
-        """
         current_folder_id = base_folder_id
         path_parts = path.split(os.sep)
         
         for part in path_parts:
-            # Check if folder exists
             query = f"name = '{part}' and '{current_folder_id}' in parents and mimeType = 'application/vnd.google-apps.folder' and trashed = false"
             results = self.service.files().list(q=query, spaces='drive', fields='files(id, name)').execute()
             folders = results.get('files', [])
@@ -47,7 +59,6 @@ class DriveUploader:
             if folders:
                 current_folder_id = folders[0]['id']
             else:
-                # Create new folder
                 folder_metadata = {
                     'name': part,
                     'mimeType': 'application/vnd.google-apps.folder',
@@ -59,9 +70,6 @@ class DriveUploader:
         return current_folder_id
 
     def upload_file(self, file_path: str, parent_folder_id: str):
-        """
-        Upload a single file to Google Drive. It's like sending a postcard, but to the cloud.
-        """
         file_name = os.path.basename(file_path)
         file_metadata = {
             'name': file_name,
@@ -72,10 +80,6 @@ class DriveUploader:
         print(f'Uploaded file: {file_name} (ID: {file.get("id")})')
 
     def upload_folder(self, local_folder: str, drive_base_folder_id: str):
-        """
-        Upload an entire folder structure to Google Drive. 
-        It's like moving your entire digital life, but with more cursing.
-        """
         for root, dirs, files in os.walk(local_folder):
             relative_path = os.path.relpath(root, local_folder)
             if relative_path == '.':
